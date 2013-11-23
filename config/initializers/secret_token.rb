2840cc65f8720f41d5744bb1c18d78ead6623ee1
laxmikanth1234 <laxmikanth.veggalam@gmail.com>
2013-11-23 13:08:26 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Navodayans::Application.config.secret_token = '91acacd1a0acf3fcc2845a892ab7f0ab81b0f19e3ac0d9580a3944e0854687cfdc654fc5252e98485ec25ec4db81e32bb21a5c6ec829c9db2b9380349ac66479'
